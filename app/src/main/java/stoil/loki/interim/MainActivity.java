d305fbd0b216602cb3d9fbc290d8c46b4b69a5e8
RainbowSuperLoki <rainbowsuperloki@gmail.com>
2023-04-05 19:40:32 +0200

    Trying to figure out spacing problem with my ImageView

--- app/src/main/java/stoil/loki/interim/MainActivity.java
+++ app/src/main/java/stoil/loki/interim/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
         //Requête de la permission COARSE LOCATION
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            //Toast.makeText(this, "Permission not granted yet", Toast.LENGTH_SHORT).show();
             // Permission is not granted
             // Request the permission
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
                     MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+        }
+        else //Permission granted
+        {
+            Toast.makeText(this, "Permission granted already", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Location permission granted, do your work here
+                } else {
+                    // Location permission denied
+                }
+                break;
+            // Handle other permissions here if needed
         }
     }
 }
