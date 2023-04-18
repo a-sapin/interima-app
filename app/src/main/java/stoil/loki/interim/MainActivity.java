ea24d96ba8c74b067c44a210f619659439515578
AlyssaShep <margay.oce@gmail.com>
2023-04-18 17:29:08 +0200

    Ajout : - d une classe SignIn pour definir le comportement lors de la connexion - d une classe Notifications pour definir le comportement pour consulter les notifications
    Modifications :
    - AndroidManifest -> ajout des nouvelles classes
    - logique des boutons du header dans Mainactivity (sign in, notifications)
    - layout MainActivity -> rajout scrollView
    - layout de connexion universel
    
    To Do :
    - finir logique des boutons de offer_item
    - faire logique pour les notifications etc etc

--- app/src/main/java/stoil/loki/interim/MainActivity.java
+++ app/src/main/java/stoil/loki/interim/MainActivity.java
@@ -4,6 +4,9 @@ import android.Manifest;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import java.io.Serializable;
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements Serializable {
 
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
     private ArrayList<Offer> offers = new ArrayList<Offer>();
+    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +62,38 @@ public class MainActivity extends AppCompatActivity implements Serializable {
             Toast.makeText(this, "Permission granted already", Toast.LENGTH_SHORT).show();
         }
 
+        for (int i = 0; i < 10; i++) {
+            offers.add(new Offer(1, "Developpeur Fullstack", "capgemini.com"));
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.offersList);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        OfferAdapter adapter = new OfferAdapter(offers);
+        recyclerView.setAdapter(adapter);
+
+        // sign in button
+        Button sign_in = findViewById(R.id.login_button);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SignIn.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
+        // notifications button
+
+        ImageButton notifications = findViewById(R.id.notification);
+        notifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Notifications.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
+        // search bar
 
     }
 
@@ -73,15 +110,6 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                 break;
             // Handle other permissions here if needed
         }
-
-        for (int i = 0; i < 10; i++) {
-            offers.add(new Offer(1, "Developpeur Fullstack", "capgemini.com"));
-        }
-
-        ListView listView = findViewById(R.id.offersList);
-        OfferAdapter adapter = new OfferAdapter(this, offers);
-        Log.d("MainActivity", "List item created");
-        listView.setAdapter(adapter);
 
     }
 }
--- app/src/main/java/stoil/loki/interim/OfferAdapter.java
+++ app/src/main/java/stoil/loki/interim/OfferAdapter.java
@@ -2,47 +2,103 @@ package stoil.loki.interim;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
-public class OfferAdapter extends ArrayAdapter<Offer> implements Serializable {
-    private ArrayList<Offer> annonces;
+public class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.ViewHolder> {
+    private List<Offer> annonces;
 
-    public OfferAdapter(Context context, ArrayList<Offer> annonces) {
-        super(context, 0, annonces);
+    public OfferAdapter(ArrayList<Offer> annonces) {
         this.annonces = annonces;
     }
 
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.offer_item, parent, false);
-        }
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.offer_item, parent, false);
+        return new ViewHolder(view);
+    }
 
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Offer offer = annonces.get(position);
-
-        TextView titleTextView = convertView.findViewById(R.id.title);
-        TextView descriptionTextView = convertView.findViewById(R.id.description);
-
-        titleTextView.setText(offer.getTitle());
-        descriptionTextView.setText(offer.getUrl());
-
-        convertView.findViewById(R.id.apply_button).setOnClickListener(new View.OnClickListener() {
+        holder.titleTextView.setText(offer.getTitle());
+        holder.descriptionTextView.setText(offer.getUrl());
+        holder.applyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Action à exécuter lorsque le bouton est cliqué
-                Intent intent = new Intent(getContext(), FullOffer.class);
-                intent.putExtra("offer", (Serializable) offer);
-                getContext().startActivity(intent);
+                Intent intent = new Intent(view.getContext(), FullOffer.class);
+                intent.putExtra("offer", offer);
+                view.getContext().startActivity(intent);
             }
         });
 
-        return convertView;
+        holder.bookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ajout/retirer bookmark de la bdd + changement de l image du bouton
+
+                Drawable imageDrawable = holder.bookmark.getDrawable();
+
+                Resources resources = holder.itemView.getContext().getResources();
+                Drawable marque = ResourcesCompat.getDrawable(resources, R.drawable.marque_page, null);
+                Drawable marque_vide = ResourcesCompat.getDrawable(resources, R.drawable.marque_page_vide, null);
+
+                if(imageDrawable.getConstantState().equals(marque.getConstantState())) {
+                    holder.bookmark.setImageDrawable(marque_vide);
+                    // retirer le bookmark de la bdd
+
+                } else {
+                    holder.bookmark.setImageDrawable(marque);
+                    // ajouter le bookmark dans la bdd
+                    
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return annonces.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView titleTextView;
+        public TextView descriptionTextView;
+        public Button applyButton;
+        public ImageButton bookmark;
+        public ImageButton translation;
+        public ImageButton dot;
+
+        public ViewHolder(View view) {
+            super(view);
+            titleTextView = view.findViewById(R.id.title);
+            descriptionTextView = view.findViewById(R.id.description);
+            applyButton = view.findViewById(R.id.apply_button);
+            bookmark = view.findViewById(R.id.bookmark);
+            translation = view.findViewById(R.id.language_button);
+            dot = view.findViewById(R.id.dot);
+        }
     }
 }
