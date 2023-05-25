a0bbe623b0ec4f7c78d58bcc3f954a1b675a8907
StOil-L <lorenzo.puccio@etu.umontpellier.fr>
2023-05-25 21:23:17 +0200

    Première implémentation partage candidature

--- app/src/main/java/stoil/loki/interim/ApplyReuseDisplay.java
+++ app/src/main/java/stoil/loki/interim/ApplyReuseDisplay.java
@@ -1,8 +1,11 @@
 package stoil.loki.interim;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.ArrayList;
 
 public class ApplyReuseDisplay extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1993;
 
     RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
 
@@ -82,4 +87,25 @@ public class ApplyReuseDisplay extends AppCompatActivity {
         });
 
     }
+
+    // Handle the permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.setData(Uri.parse("sms:"));
+                String smsbody = "Partagé via Interima: TITRE, DATE, " +
+                        "LIEN CV, LIEN LM";
+                sendIntent.putExtra("sms_body", smsbody);
+                startActivity(sendIntent);
+            } else {
+                Toast.makeText(getApplicationContext(), "Activez l'accès à vos messages pour " +
+                        "partager cette annonce par SMS.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
