4aac7b06a0892b4f074517a38e2511e7058b83c9
RainbowSuperLoki <rainbowsuperloki@gmail.com>
2023-05-25 22:16:26 +0200

    Additions to Offer class

--- app/src/main/java/stoil/loki/interim/Offer.java
+++ app/src/main/java/stoil/loki/interim/Offer.java
@@ -10,6 +10,8 @@ public class Offer implements Serializable {
     float geolat;
     float geolong;
 
+    double distFromUser;
+
 
     public Offer(int id, String title, String url) {
         this.id = id;
@@ -19,12 +21,21 @@ public class Offer implements Serializable {
         //DEFAULT VALUES, THIS ISN'T FINAL /!\
         this.geolat = (float) 48.856614;
         this.geolong = (float) 2.3522219;
+        this.distFromUser = 0;
     }
 
     public Offer() {
         this.id = 0;
         this.title = "titre de l'annonce";
         this.url = "url de l'annonce";
+    }
+
+    public double getDist(float x1, float y1)
+    {
+        float deltaX = this.geolong - x1;
+        float deltaY = this.geolat - y1;
+        double distance = Math.sqrt(((double) deltaX * (double) deltaX) + ((double) deltaY * (double) deltaY));
+        return distance;
     }
 
     public int getId() {
@@ -43,4 +54,19 @@ public class Offer implements Serializable {
         this.url = url;
     }
 
+    public float getGeolat() {
+        return geolat;
+    }
+
+    public void setGeolat(float geolat) {
+        this.geolat = geolat;
+    }
+
+    public float getGeolong() {
+        return geolong;
+    }
+
+    public void setGeolong(float geolong) {
+        this.geolong = geolong;
+    }
 }
