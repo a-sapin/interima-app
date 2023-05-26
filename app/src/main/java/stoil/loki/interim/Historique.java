70d29be6c47a6b023df6a63b9eb94f1754f79153
StOil-L <lorenzo.puccio@etu.umontpellier.fr>
2023-05-26 22:57:32 +0200

    Début d'implémentation historique recherche

---
+++ app/src/main/java/stoil/loki/interim/Historique.java
@@ -0,0 +1,14 @@
+package stoil.loki.interim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Historique extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.historique);
+    }
+}
