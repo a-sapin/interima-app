4c2af0194b2624f96e1a73d86f57c912f2ae0225
RainbowSuperLoki <rainbowsuperloki@gmail.com>
2023-05-27 17:02:54 +0200

    Draft of new create_offer.xml layout

---
+++ app/src/main/java/stoil/loki/interim/CreateOffer.java
@@ -0,0 +1,108 @@
+package stoil.loki.interim;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+public class CreateOffer extends AppCompatActivity {
+
+    private Offer offer;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apply);
+
+        Button apply = findViewById(R.id.modifProfil);
+
+        // verifier que tous les champs sont remplis avant de transmettre le apply
+        // mettre un Toast avec le fait que tous les champs ne sont pas remplis par exemple
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                Toast.makeText(getApplicationContext(), "Candidature envoyée", Toast.LENGTH_SHORT).show();
+                view.getContext().startActivity(intent);
+            }
+        });
+
+        BottomNavigationView menu = findViewById(R.id.navigation);
+        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Gérez la redirection ici
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        // Redirigez vers l'écran d'accueil
+
+                        Intent intenth = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intenth);
+                        return true;
+
+                    case R.id.favoris:
+
+                        Intent intentf = new Intent(getApplicationContext(), Bookmarks.class);
+                        startActivity(intentf);
+                        return true;
+
+                    case R.id.recherche:
+                        Intent intents = new Intent(getApplicationContext(), RecherchePage.class);
+                        startActivity(intents);
+                        return true;
+                    case R.id.notifs:
+
+                        Intent intentn = new Intent(getApplicationContext(), Notifications.class);
+                        startActivity(intentn);
+                        return true;
+
+                    case R.id.profil:
+                        // si connecter donner la page du profil
+                        // sinon on demande la co ou inscription
+
+                        if(true) {
+                            Intent intentp = new Intent(getApplicationContext(), ProfilDisplay.class);
+                            startActivity(intentp);
+                        } else {
+                            Intent intentp = new Intent(getApplicationContext(), SignIn.class);
+                            startActivity(intentp);
+                        }
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+
+    }
+
+    public ArrayList<String> getInfoToken() {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("User DATA", Context.MODE_PRIVATE);
+        ArrayList<String> value = new ArrayList<>();
+        value.add(sharedPreferences.getString("role", null));
+        value.add(sharedPreferences.getString("id", null));
+
+        return value;
+    }
+
+    public String getInfoTokenID() {
+        ArrayList<String> info = getInfoToken();
+        return info.get(1);
+    }
+
+    public String getInfoTokenRole() {
+        ArrayList<String> info = getInfoToken();
+        return info.get(0);
+    }
+}
