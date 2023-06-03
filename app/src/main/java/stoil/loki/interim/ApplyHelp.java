7249e8389c90f512e46b82dd9704d19ed3d468ef
StOil-L <lorenzo.puccio@etu.umontpellier.fr>
2023-06-03 18:40:49 +0200

    ApplyHelp barre navigation définitive

--- app/src/main/java/stoil/loki/interim/ApplyHelp.java
+++ app/src/main/java/stoil/loki/interim/ApplyHelp.java
@@ -3,7 +3,9 @@ package stoil.loki.interim;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
@@ -13,6 +15,8 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class ApplyHelp extends AppCompatActivity {
 
@@ -116,6 +120,25 @@ public class ApplyHelp extends AppCompatActivity {
         super.onResume();
         BottomNavigationView menu = findViewById(R.id.navigation);
         menu.getMenu().findItem(R.id.profil).setChecked(true);
+    }
+
+    public ArrayList<String> getInfoToken() {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("User DATA", Context.MODE_PRIVATE);
+        ArrayList<String> value = new ArrayList<>();
+        value.add(sharedPreferences.getString("role", null));
+        value.add(sharedPreferences.getString("id", null));
+
+        return value;
+    }
+
+    public String getInfoTokenID() {
+        ArrayList<String> info = getInfoToken();
+        return info.get(1);
+    }
+
+    public String getInfoTokenRole() {
+        ArrayList<String> info = getInfoToken();
+        return info.get(0);
     }
 
 }
