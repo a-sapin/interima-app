3db704d3ea14ea9c59d4227e3a8d452d5e5a8727
StOil-L <lorenzo.puccio@etu.umontpellier.fr>
2023-06-04 00:48:17 +0200

    Début page upload JSON + mini changement géoloc

---
+++ app/src/main/java/stoil/loki/interim/UploadOffer.java
@@ -0,0 +1,127 @@
+package stoil.loki.interim;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+public class UploadOffer extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.upload_offer);
+
+        Button select = findViewById(R.id.select);
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileBrowser();
+            }
+        });
+
+        Button upload = findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Behavior to send data
+            }
+        });
+
+        BottomNavigationView menu = findViewById(R.id.navigation);
+        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        Intent intenth = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intenth);
+                        return true;
+                    case R.id.favoris:
+                        if(((UploadOffer)menu.getContext()).getInfoTokenID() != null) {
+                            Intent intentf = new Intent(getApplicationContext(), Bookmarks.class);
+                            startActivity(intentf);
+                            return true;
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Connectez-vous pour " +
+                                    "accéder à cette fonctionnalité.", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case R.id.recherche:
+                        Intent intents = new Intent(getApplicationContext(), RecherchePage.class);
+                        startActivity(intents);
+                        return true;
+                    case R.id.notifs:
+                        if(((UploadOffer)menu.getContext()).getInfoTokenID() != null) {
+                            Intent intentn = new Intent(getApplicationContext(), Notifications.class);
+                            startActivity(intentn);
+                            return true;
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Connectez-vous pour " +
+                                    "accéder à cette fonctionnalité.", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case R.id.profil:
+                        Intent intentp = new Intent(getApplicationContext(), ProfilDisplay.class);
+                        startActivity(intentp);
+                        return true;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void openFileBrowser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("file/json");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            TextView jsoncontent = (TextView) findViewById(R.id.jsoncontent);
+            jsoncontent.setText(uri.toString());
+        }
+    }
+
+    public ArrayList<String> getInfoToken() {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("User DATA", Context.MODE_PRIVATE);
+        ArrayList<String> value = new ArrayList<>();
+        value.add(sharedPreferences.getString("role", null));
+        value.add(sharedPreferences.getString("id", null));
+
+        return value;
+    }
+
+    public String getInfoTokenID() {
+        ArrayList<String> info = getInfoToken();
+        return info.get(1);
+    }
+
+    public String getInfoTokenRole() {
+        ArrayList<String> info = getInfoToken();
+        return info.get(0);
+    }
+
+}
